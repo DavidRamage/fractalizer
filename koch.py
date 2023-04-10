0b3c798d10d9faef4f9274f0a024486cd345b66a
David Ramage <david.p.ramage@gmail.com>
2023-04-09 17:39:50 -0700

    First iteration of Koch snowflake code

---
+++ koch.py
@@ -0,0 +1,31 @@
+import click
+import turtle
+@click.command()
+@click.option('--iterations','-i',default=3)
+def koch(iterations):
+  print(iterations)
+  turtle.speed('fastest')
+  
+  # adjust size as desired
+  size = 300
+  
+  # move to starting position
+  turtle.penup()
+  turtle.goto(-size/2, size/4)
+  turtle.pendown()
+  
+  # draw Koch snowflake
+  for i in range(iterations):
+      koch_snowflake(size, 4)
+      turtle.right(120)
+  
+  turtle.done()
+
+def koch_snowflake(length, depth):
+    if depth == 0:
+        turtle.forward(length)
+    else:
+        for angle in [60, -120, 60, 0]:
+            koch_snowflake(length / 3, depth - 1)
+            turtle.left(angle)
+
