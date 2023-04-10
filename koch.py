3438887658aa4a69778c12a8934114fce732d391
David Ramage <david.p.ramage@gmail.com>
2023-04-09 20:23:20 -0700

    Fix indentatoin

--- koch.py
+++ koch.py
@@ -1,25 +1,20 @@
-import click
 import turtle
+import click
 @click.command()
 @click.option('--iterations','-i',default=3)
 def koch(iterations):
-  print(iterations)
-  turtle.speed('fastest')
-  
-  # adjust size as desired
-  size = 300
-  
-  # move to starting position
-  turtle.penup()
-  turtle.goto(-size/2, size/4)
-  turtle.pendown()
-  
-  # draw Koch snowflake
-  for i in range(iterations):
-      koch_snowflake(size, 4)
-      turtle.right(120)
-  
-  turtle.done()
+    turtle.speed('fastest')
+    # adjust size as desired
+    size = 300
+    # move to starting position
+    turtle.penup()
+    turtle.goto(-size/2, size/4)
+    turtle.pendown()
+    # draw Koch snowflake
+    for i in range(iterations):
+        koch_snowflake(size, 4)
+        turtle.right(120)
+    turtle.done()
 
 def koch_snowflake(length, depth):
     if depth == 0:
@@ -28,4 +23,3 @@ def koch_snowflake(length, depth):
         for angle in [60, -120, 60, 0]:
             koch_snowflake(length / 3, depth - 1)
             turtle.left(angle)
-
